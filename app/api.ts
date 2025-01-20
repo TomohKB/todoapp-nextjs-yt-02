50e897331271d90b3bb4b9e2be3ee3cae0148671
kubotomohiro <tomoh.ku0013@gmail.com>
2025-01-20 22:30:12 +0900

    #01 35:25から

---
+++ app/api.ts
@@ -0,0 +1,23 @@
+import { Task } from "./types";
+
+export const getAllTodos = async (): Promise<Task[]> => {
+  const res = await fetch(`http://localhost:3002/tasks`, {
+    cache: "no-store",
+  });
+  const todos = res.json();
+
+  return todos;
+};
+
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(`http://localhost:3002/tasks`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
+
+  return newTodo;
+};
